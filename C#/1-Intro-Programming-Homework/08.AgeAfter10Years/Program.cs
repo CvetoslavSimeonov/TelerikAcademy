7b5eec6a5f4dbd1b53d48cb1c16f3d8a605c5a13
CvetoslavSimeonov <cvetoslav8@gmail.com>
2016-08-12 17:59:49 +0200

    1-Intro-Programming-Homework

--- C#/1-Intro-Programming-Homework/08.AgeAfter10Years/Program.cs
+++ C#/1-Intro-Programming-Homework/08.AgeAfter10Years/Program.cs
@@ -4,6 +4,11 @@ class AgeAfter10Years
 {
     static void Main()
     {
+        int c = int.Parse(Console.ReadLine());
+        c = c + 100;
+        string a = Console.ReadLine();
+        Console.WriteLine(a);
+        Console.WriteLine(c);
 
     }
 }
